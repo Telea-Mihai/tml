fbe2ea0e8cc207a3021242c8d2843258cb494b0e
Mihai <mihaitelea02@gmail.com>
2025-11-10 21:48:29 +0200

    made basic next js project, started work on implementing the home page

---
+++ tml/src/app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <main className="homeLayout">
+      <div className="Menu">
+        <div>
+          <h1>&gt;Menu</h1>
+          <h2>Please make a selection:</h2>
+        </div>
+      </div>
+      <div className="Machine">
+        <div className="Upper">
+
+        </div>
+        <div className="Lower">
+          <div className="Screen">
+            
+          </div>
+        </div>
+      </div>
+
+    </main>
+  )
+}
